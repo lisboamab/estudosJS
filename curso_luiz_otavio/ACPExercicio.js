6eb4bf211236264b4e97b0d2fc023e9daf9c7333
lisboamab <lisboamab@gmail.com>
2022-06-11 17:42:15 -0300

    aulas de strings e variaveis

---
+++ curso_luiz_otavio/ACPExercicio.js
@@ -0,0 +1,6 @@
+let num1 = Number(prompt('Digite o número 1: '))
+let num2 = Number(prompt('Digite o número 2: '))
+
+let soma = num1 + num2
+
+alert(`A soma dos dois números passados é de ${soma}`)
---
+++ curso_luiz_otavio/variaveisExercicios.js
@@ -0,0 +1,11 @@
+let vA = 'A' //B
+let vB = 'B' //C
+let vC = 'C' //A
+
+const vTemp = vA
+vA = vB
+vB = vC
+vC = vTemp
+
+
+console.log(vA, vB, vC)
